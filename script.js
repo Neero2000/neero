46a37acb8d53fb48749d106d010fdda7ca13cda7
Neero2000 <109559968+Neero2000@users.noreply.github.com>
2023-11-27 21:35:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Golna tlovini besfif 3lik FHAMTIIII!!!!!!";
+  question.innerHTML = "Golna tlovini besfif 3lik!!!!!!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
